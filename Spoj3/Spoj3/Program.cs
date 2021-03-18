8a860e4b57f3dfab48404351a533a38790b46e3d
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-03-18 09:28:23 +0100

    Ćwiczenie3

---
+++ Spoj3/Spoj3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Spoj3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            while (true)
+            {
+                string napis = Console.ReadLine();
+                if (string.IsNullOrEmpty(napis)) ;
+                {
+                    break;
+                }
+
+                int liczba = int.Parse(napis);
+                suma += liczba;
+                Console.WriteLine(suma);
+
+            }
+        }
+    }
+}
