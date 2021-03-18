561d084e6ff3b4e97e516768eb14774eac9ce660
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-03-18 09:41:52 +0100

    Ćwiczenie 4

---
+++ Spoj4/Spoj4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Spoj4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+                string napis = Console.ReadLine();
+                var tab = napis.Split(' ');
+                int suma = 0;
+                for (int j = 0; j < n; j++) ;
+                {
+                    int j = 0;
+                    int x = int.Parse(tab[j]);
+                    suma += x;
+                }
+                Console.WriteLine(suma);
+            }
+
+        }
+    }
+}
