57525b10beb8708657e98b4a816992759ecf40d0
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-03-18 09:20:26 +0100

    Ćwiczenie 2

---
+++ Spoj2/Spoj2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Suma
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Wprowadz Liczby:");
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int Suma = A + B + C;
+            Console.WriteLine($"Suma: {Suma}");
+
+        }
+    }
+}
