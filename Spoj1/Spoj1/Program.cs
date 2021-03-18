5d06ef8ca6630095d3c97eeee3947fb9a91a1f55
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-03-18 09:15:03 +0100

    Ćwiczenie1

---
+++ Spoj1/Spoj1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Spoj1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int suma = a + b;
+            Console.WriteLine(suma);
+
+        }
+    }
+}
