955087ba8b168f15df85ae0d09a1d921a84fd811
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-03-30 13:48:50 +0200

    idk

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    if (i != j)
+                    {
+                        n--;
+                    }
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
