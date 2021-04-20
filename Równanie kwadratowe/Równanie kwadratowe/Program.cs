cf3852809e761c33d35a077030327fd2a88751d0
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-04-20 11:23:04 +0200

    Koniec

---
+++ Równanie kwadratowe/Równanie kwadratowe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Równanie_kwadratowe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SMPSEQ6/SMPSEQ6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPSEQ6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Dane = Console.ReadLine();
+            string S = Console.ReadLine();
+            string Q = Console.ReadLine();
+            string[] DaneSplit = Dane.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] SSplit = S.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] QSplit = Q.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int n = Int32.Parse(DaneSplit[0]);
+            int x = Int32.Parse(DaneSplit[1]);
+            if (x>=0)
+            {
+                for (int i = 0; i<n; ++i)
+                {
+                    for (int y = -x; y <= x; ++y)
+                    {
+                        if (i+y<n && i+y>=0 && SSplit[i] == QSplit[i + y])
+                        {
+                            Console.Write($"{i + 1}" + " ");
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("X ujemny, blad");
+            }
+        }
+    }
+}
