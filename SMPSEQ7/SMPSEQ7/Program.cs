d9bee1b92af9b0e6bb4a7bcfaad5cf4b4b6bc928
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-04-20 12:58:57 +0200

    Koniec

---
+++ SMPSEQ7/SMPSEQ7/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPSEQ7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n = Console.ReadLine();
+            string S = Console.ReadLine();
+            int a = int.Parse(n);
+            string[] SSplit = S.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] Dane = Array.ConvertAll<string, int>(SSplit, int.Parse);
+            int b = 0;
+            int zmiana = 0;
+            for (int i = 0; i < a - 1; i++)
+            {
+                if (Dane[i] > Dane[i+1])
+                {
+                    b++;
+                }
+                else if (b == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    b++;
+                    zmiana++;
+                    break;
+                }
+            }
+            for (int k = 0 + b; k < a - 1; k++) 
+            {
+                if (Dane[k] < Dane[k + 1])
+                {
+                    b++;
+                }
+                else
+                {
+                    b++;
+                    break;
+                }
+            }
+            if (b == a-1 && zmiana == 1)
+            {
+                Console.Write("Yes");
+            }
+            else
+            {
+                Console.Write("No");
+            }
+        }
+    }
+}
