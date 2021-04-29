039bedf01b69e612d33d7a9927fdb4e7db14a139
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-04-29 15:45:01 +0200

    Koniec

--- HFLOOR - Hotel Floors/HFLOOR - Hotel Floors/Program.cs
+++ HFLOOR - Hotel Floors/HFLOOR - Hotel Floors/Program.cs
@@ -11,12 +11,12 @@ namespace HFLOOR___Hotel_Floors
         static void Main(string[] args)
         {
             int NOTC = int.Parse(Console.ReadLine());
-            for (int z = 0; z < NOTC; z++)
+            for (int b = 0; b < NOTC; b++)
             {
                 string[] input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
                 int M = int.Parse(input[0]);
                 int N = int.Parse(input[1]);
-                char[][] arr = new char[N][];
+                char[][] arr = new char[M][];
                 for (int i = 0; i < M; i++)
                 {
                     arr[i] = new char[N];
