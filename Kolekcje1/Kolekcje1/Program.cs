4ef48bfb2bc55643ab28840d6a83ae657ba19a19
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-04-29 09:47:09 +0200

    Począten końca

---
+++ Kolekcje1/Kolekcje1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolekcje1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> zwierzaki = new List<string>();
+            zwierzaki.Add("pies");
+            zwierzaki.Add("ryba");
+            zwierzaki.Add("małpa");
+            zwierzaki.Add("ptak");
+            if(zwierzaki.Contains("krowa"))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+            zwierzaki.Reverse();
+            zwierzaki.Sort();
+            Console.WriteLine(string.Join(", ", zwierzaki));
+        }
+    }
+}
