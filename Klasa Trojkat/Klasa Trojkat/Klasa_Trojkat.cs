c1cf84e681a0890c55ba1909641ca7345a5ea3f8
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-05-06 09:43:41 +0200

    Zajęcia

---
+++ Klasa Trojkat/Klasa Trojkat/Klasa_Trojkat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_Trojkat
+{
+    class Klasa_Trojkat
+    {
+        public const int PRECYZJA = 4;
+        // Dane
+        public double A { get; private set; }
+
+        public double B { get; private set; }
+
+        public double C { get; private set; }
+
+        // Konstruktory
+        public Klasa_Trojkat()
+        {
+            A = B = C = 1.0;
+        }
+
+        public Klasa_Trojkat(double a, double b, double c)
+        {
+            a = Math.Round(a, PRECYZJA);
+            b = Math.Round(b, PRECYZJA);
+            c = Math.Round(c, PRECYZJA);
+
+            if (a < 0 || b < 0 || c < 0)
+                throw new ArgumentException("Długości nie mogą być ujemne");
+            if (a + b < c || a + c < b || b + c < a)
+                throw new ArgumentException("Nie spełnipny warunek trójkąta");
+            A = a;
+            B = b;
+            C = c;
+        }
+        // To String
+        public override string ToString()
+        {
+            return $"Trojkat (a={A}, b={B}, c={C})";
+        }
+        // Inne metody
+    }
+}
---
+++ Klasa Trojkat/Klasa Trojkat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klasa_Trojkat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Klasa_Trojkat t;
+            t = new Klasa_Trojkat(3, 4, 5);
+            Console.WriteLine(t.ToString());
+            Klasa_Trojkat domyslny = new Klasa_Trojkat();
+            Console.WriteLine(domyslny);
+        }
+    }
+}
