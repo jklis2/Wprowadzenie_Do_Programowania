796216130daf015820b08393d1d6eed4296ffa50
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-05-13 07:45:03 +0200

    Koniec

---
+++ KM002Z01 - Objętość stożka/KM002Z01 - Objętość stożka/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KM002Z01___Objętość_stożka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            long R = long.Parse(input[0]);
+            long L = long.Parse(input[1]);
+            decimal V = 0;
+
+            if (R < 0 || L < 0)
+            {
+                Console.WriteLine("Ujemny argument");
+                return;
+            }
+            try
+            {
+                V = obj(R, L);
+                Console.WriteLine($"{Math.Floor(V)} {Math.Ceiling(V)}");
+            }
+            catch (ArithmeticException)
+            {
+                Console.WriteLine("Obiekt nie istnieje");
+            }
+        }
+        static decimal obj(long R, long L)
+        {
+            double H = Math.Sqrt(-(R * R) + (L * L));
+            if (double.IsNaN(H))
+            {
+                throw new ArithmeticException();
+            }
+            return ((decimal)Math.PI * R * R * (decimal)H) / 3;
+        }
+    }
+}
