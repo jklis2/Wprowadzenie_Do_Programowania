cba3850bb9c661b8a31e21847b73b691e9cddced
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-05-14 15:21:08 +0200

    Koniec

--- Zadanie Równanie kwadratowe/Zadanie Równanie kwadratowe/Program.cs
+++ Zadanie Równanie kwadratowe/Zadanie Równanie kwadratowe/Program.cs
@@ -8,8 +8,39 @@ namespace Zadanie_Równanie_kwadratowe
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void QuadraticEquation(int a, int b, int c)
         {
+            double wynik = ((double)b * b) - (4 * (double)a * c);
+            if (a == 0 && b == 0 && c == 0)
+            {
+                Console.WriteLine("infinity");
+                return;
+            }
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("empty");
+                return;
+            }
+            if (a == 0)
+            {
+                Console.WriteLine($"x={-((double)c / b):F2}");
+                return;
+            }
+            if (Math.Round(wynik, 2) > 0)
+            {
+                double x1 = (-b - Math.Sqrt(wynik)) / (2 * (double)a);
+                double x2 = (-b + Math.Sqrt(wynik)) / (2 * (double)a);
+                Console.WriteLine($"x1={x1:F2}");
+                Console.WriteLine($"x2={x2:F2}");
+                return;
+            }
+            if (Math.Round(wynik, 2) == 0)
+            {
+                double x = (-b) / (2 * (double)a);
+                Console.WriteLine($"x={x:F2}");
+                return;
+            }
+            Console.WriteLine("empty");
         }
     }
 }
