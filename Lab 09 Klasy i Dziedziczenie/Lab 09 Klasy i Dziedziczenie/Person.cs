d876560c131f705acc2cea89cb35ae5bedcfa9d3
jklis2 <80206032+jklis2@users.noreply.github.com>
2021-05-20 09:24:36 +0200

    Początek

---
+++ Lab 09 Klasy i Dziedziczenie/Lab 09 Klasy i Dziedziczenie/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_09_Klasy_i_Dziedziczenie
+{
+    class Person
+    {
+    }
+}
---
+++ Lab 09 Klasy i Dziedziczenie/Lab 09 Klasy i Dziedziczenie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_09_Klasy_i_Dziedziczenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
